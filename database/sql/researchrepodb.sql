a18339b2132047e41dc6205c94dfd5a172dbe644
ow3ndesu <enguerraowen@gmail.com>
2023-05-11 05:27:44 +0800

    database

---
+++ database/sql/researchrepodb.sql
@@ -0,0 +1,221 @@
+-- MariaDB dump 10.18  Distrib 10.4.17-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: researchrepodb
+-- ------------------------------------------------------
+-- Server version	8.0.25
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `admins`
+--
+
+DROP TABLE IF EXISTS `admins`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `admins` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `admin_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `user_id` int NOT NULL,
+  `firstname` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `middlename` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `lastname` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `address` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `contact_no` varchar(11) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `is_completed` tinyint(1) NOT NULL DEFAULT '0',
+  `created_at` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idx_studentid` (`admin_id`),
+  KEY `admins_ibfk_1` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `admins`
+--
+
+LOCK TABLES `admins` WRITE;
+/*!40000 ALTER TABLE `admins` DISABLE KEYS */;
+INSERT INTO `admins` VALUES (1,'ADMIN000001',1,'Jon','Admin','Doe','Somewhere St.','09123456789',1,'10/19/2022');
+/*!40000 ALTER TABLE `admins` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `faculties`
+--
+
+DROP TABLE IF EXISTS `faculties`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `faculties` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `faculty_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `user_id` int NOT NULL,
+  `firstname` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `middlename` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `lastname` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `address` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `contact_no` varchar(11) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `is_completed` tinyint(1) NOT NULL DEFAULT '0',
+  `created_at` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idx_studentid` (`faculty_id`) USING BTREE,
+  KEY `admins_ibfk_1` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `faculties`
+--
+
+LOCK TABLES `faculties` WRITE;
+/*!40000 ALTER TABLE `faculties` DISABLE KEYS */;
+INSERT INTO `faculties` VALUES (4,'FACULTY000SYFDG9',19,'Jane','Faculty','Does','Somewhere PH.','09123456789',1,'05/06/2023');
+/*!40000 ALTER TABLE `faculties` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `researches`
+--
+
+DROP TABLE IF EXISTS `researches`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `researches` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `research_id` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `copy` varchar(255) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `author` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `status` varchar(45) NOT NULL COMMENT 'AVAILABLE, UNAVAILABLE',
+  `remarks` varchar(45) NOT NULL DEFAULT '-',
+  `inserted_by` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '-',
+  `inserted_at` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idx_researchid` (`research_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `researches`
+--
+
+LOCK TABLES `researches` WRITE;
+/*!40000 ALTER TABLE `researches` DISABLE KEYS */;
+INSERT INTO `researches` VALUES (5,'RESE000I7FQNH','RESE000I7FQNH-Mananghaya, Jerick M..png','RESE000I7FQNH-Mananghaya, Jerick M..pdf','A Web-Based Research Repository System for the Student and Faculty Members of Nueva Ecija University of Science and Technology Municipality Government of Talavera (MGT)','Mananghaya, Jerick M.','A Web-Based Research Repository System','ACTIVE','-','ADMIN000001','05/11/2023');
+/*!40000 ALTER TABLE `researches` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `students`
+--
+
+DROP TABLE IF EXISTS `students`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `students` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `student_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `user_id` int NOT NULL,
+  `firstname` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `middlename` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `lastname` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `address` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `contact_no` varchar(11) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `is_completed` tinyint(1) NOT NULL DEFAULT '0',
+  `created_at` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idx_studentid` (`student_id`),
+  KEY `students_ibfk_1` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `students`
+--
+
+LOCK TABLES `students` WRITE;
+/*!40000 ALTER TABLE `students` DISABLE KEYS */;
+INSERT INTO `students` VALUES (1,'STUD0003IAVZX',4,'JUAN','PABLO','DELA CRUZ','Pag-Asa','0905985511',1,'10/30/2022'),(3,'STUD000R4PVCS',6,'JON','STARK','SNOW','Winterfell','09067894410',1,'11/03/2022'),(5,'STUD000FCE17D',7,'LUIS','','SAN JUAN','SOMEHEREST.','09123456789',1,'11/26/2022'),(7,'STUD000UCPR37',9,'JUNNIE','ORCHIDS','LUISA','STO DOMINGO','09123456789',1,'11/26/2022'),(8,'STUD0001BJVNF',10,'MEME','','EMAM','SOMEWHERE ST.','09123456789',1,'12/01/2022'),(9,'STUD0000EY4GA',13,'TEST','TEST','TEST','TEST','09123456789',1,'05/05/2023');
+/*!40000 ALTER TABLE `students` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `receiver` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `proof` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `user_type` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `status` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `created_at` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `idx_email` (`username`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'@adminacc','test@gmail.com','5b94c55fc69d8d32275862a930ebb46f','@student4-536.jpg','ADMIN','ENABLED','10/19/2022'),(4,'@student2','zidsaadad@gmail.com','2c1ddc2fc774a31cbe18e8b3575d62cc','@student4-536.jpg','STUDENT','ENABLED','10/30/2022'),(6,'@student3','enguerra1111@gmail.com','80ec8e02a3987d28fe68894658400e8c','@student4-536.jpg','STUDENT','ENABLED','11/03/2022'),(7,'@student1','test@gmail.com','fe96821ecf1191932f631d60e620e466','@student4-536.jpg','STUDENT','ENABLED','11/24/2022'),(9,'@student4','test@gmail.com','4d53590c0a24c82a7530d1eeccae9558','@student4-256.png','STUDENT','ENABLED','11/26/2022'),(10,'@student5','test@gmail.com','81778092f0802a496a5990666db10095','@student5-1570.jpg','STUDENT','ENABLED','11/29/2022'),(11,'@student7','test@gmail.com','9888ae92687cfb6e65c31acc7aef0ca8','@student7-1293.jpg','STUDENT','DISABLED','12/01/2022'),(13,'@student9','student909@email.com','6f805ce7b5bb30a4d337cbc216f1f12d','@student9-1535.jpg','STUDENT','ENABLED','05/05/2023'),(19,'@facultyacc2','email@test.com','83e301bbefc78af5a787293864b46b15','-','FACULTY','ENABLED','05/06/2023'),(21,'@student8','student909@email.com','330f2522d1267fcf46faef31280021cd','@student8-1896.jpg','STUDENT','DISABLED','05/06/2023');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `verifier`
+--
+
+DROP TABLE IF EXISTS `verifier`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `verifier` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `email` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `otp` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `is_expired` tinyint(1) NOT NULL DEFAULT '0',
+  `created_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=168 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `verifier`
+--
+
+LOCK TABLES `verifier` WRITE;
+/*!40000 ALTER TABLE `verifier` DISABLE KEYS */;
+INSERT INTO `verifier` VALUES (167,'enguerra1111@gmail.com','273508',1,'2023-05-07 23:47:35');
+/*!40000 ALTER TABLE `verifier` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-05-11  5:26:02
